b481d179caa9d1d2919e9dc4ffc7238b203a3d23
Ksenia Egorenko <kseniaegorenko66949@gmail.com>
2025-03-11 17:43:36 +0700

    Добавлены ограничения в безопасности ввода

--- src/view/editing-form-view.js
+++ src/view/editing-form-view.js
@@ -85,7 +85,7 @@ function createEditingFormTemplate(waypoint, waypointDestination, waypointOffers
               <span class="visually-hidden">Price</span>
               &euro;
             </label>
-            <input class="event__input event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice}">
+            <input class="event__input event__input--price" id="event-price-1" type="number" name="event-price" value="${basePrice}">
           </div>
 
           <button class="event__save-btn btn btn--blue" type="submit">Save</button>
